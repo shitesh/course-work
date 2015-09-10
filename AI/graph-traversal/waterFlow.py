1508526a6688ce00626b61bdab1e6a823e4bc5fb
shitesh <shiteshsaurav@gmail.com>
2015-09-10 01:25:33 -0700

    tough part solved - added class definition for priority queue required here

--- AI/graph-traversal/waterFlow.py
+++ AI/graph-traversal/waterFlow.py
@@ -2,6 +2,63 @@ import argparse
 import os
 from collections import deque
 
+class BFSPriorityQueue(object):
+    def __init__(self):
+        self.node_dict = {}
+        self.cost_dict = {}
+        self.cost_list = []
+
+    def update_costs(self, node_name, path_cost):
+        if path_cost in self.cost_dict:
+            self.cost_dict[path_cost].append(node_name)
+            self.cost_dict[path_cost].sort()
+        else:
+            self.cost_dict[path_cost] = [node_name]
+            self.cost_list.append(path_cost)
+            self.cost_list.sort()
+
+    def insert(self, node_name, path_cost):
+        if node_name in self.node_dict:
+            current_cost = self.node_dict[node_name]
+            if path_cost < current_cost:
+                self.node_dict[node_name] = path_cost
+
+                #update cost_dict and cost list with latest path costs
+                self.update_costs(node_name, path_cost)
+
+                # remove node name from current cost value in cost_list
+                node_list = self.cost_dict[current_cost]
+                node_list.delete(node_name)
+                if not node_list:
+                    del(self.cost_dict[current_cost])
+                    self.cost_list.remove(current_cost)
+                else:
+                    self.cost_dict[current_cost] = node_list
+        else:
+            self.node_dict[node_name] = path_cost
+            self.update_costs(node_name, path_cost)
+
+    def pop_next(self):
+        if self.cost_list:
+            cost = self.cost_list[0]
+            node_name_list = self.cost_dict.get(cost)
+            node_name = node_name_list[0]
+            del(self.node_dict[node_name])
+            del(node_name_list[0])
+
+            if node_name_list:
+                self.cost_dict[cost] = node_name_list
+            else:
+                del(self.cost_list[0])
+                del(self.cost_dict[cost])
+            return node_name
+
+        return None
+
+def perform_ucs():
+    #todo: what if the total cost of two paths become same
+    pass
+
 def perform_dfs(test_dict):
     stack = deque([])
     visited_list = []
@@ -58,10 +115,6 @@ def perform_bfs(test_dict):
     # program reached here means parsing is over and none of the goal states are reached
     return None, -1
 
-def perform_ucs():
-    #todo: what if the total cost of two paths become same
-    pass
-
 def read_file(file_obj):
     # handle exception here
     test_case_list = []
@@ -74,8 +127,8 @@ def read_file(file_obj):
 
         num_pipes = int(file_obj.readline().strip())
         count = num_pipes
-        adjancency_dict = {}
-
+        adjacency_dict = {}
+        off_period_dict = {}
         # different dictionary pattern for dfs/bfs and ucs as in usc path cost and timings does not matter
         if method_name in ['dfs', 'bfs']:
             while count > 0:
@@ -83,14 +136,40 @@ def read_file(file_obj):
                 start_node = line_parts[0]
                 end_node = line_parts[1]
 
-                start_node_list = adjancency_dict.setdefault(start_node, [])
+                start_node_list = adjacency_dict.setdefault(start_node, [])
                 start_node_list.append(end_node)
-                start_node_list.sort()
+                if method_name == 'dfs':
+                    start_node_list.sort(reverse=True)
+                else:
+                    start_node_list.sort()
                 count -= 1
+
+        elif method_name == 'ucs':
+            while count>0:
+                line_parts = file_obj.readline().strip().split(' ')
+                start_node = line_parts[0]
+                end_node = line_parts[1]
+
+                path_length = int(line_parts[2])
+                num_off_periods = int(line_parts[3])
+                off_period_list = []
+                if num_off_periods:
+                    for off_period in line_parts[4:]:
+                        start_time, end_time = map(int, off_period.split('-'))
+                        off_period_list.append((start_time, end_time))
+
+                off_period_list.sort()
+                off_period_dict[(start_node, end_node)] = off_period_list
+
+                start_node_list = adjacency_dict.setdefault(start_node, [])
+                start_node_list.append((end_node, path_length))
+                count -= 1
+
 
         start_time = int(file_obj.readline().strip())
         test_case_dict = {'method_name': method_name, 'source_node': source_node, 'destination_node_list': destination_node_list,
-                          'middle_node_list': middle_node_list, 'start_time': start_time, 'adjacency_dict': adjancency_dict
+                          'middle_node_list': middle_node_list, 'start_time': start_time, 'adjacency_dict': adjacency_dict,
+                          'off_period_dict': off_period_dict
         }
 
         test_case_list.append(test_case_dict)
